ea8e5cdf2ddbf135c1f6984b436a86f115144bf1
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-01-28 18:34:11 -0800

    Fix past push

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+
+import InputForm from './components/input_form';
+
+import './index.css';
+// import App from './App'; // following tutorial
+import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
   render() {
+    const { visible } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Sidebar.Pushable as={Segment}>
+          <Button onClick={this.toggleVisibility}>
+            <Icon name='align justify' />
+          </Button>
+          <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item >
+              <Icon name='align justify' onClick={this.toggleVisibility}/>
+            </Menu.Item>
+            <Menu.Item name='home'>
+              Home
+            </Menu.Item>
+            <Menu.Item name='blog'>
+              Blog
+            </Menu.Item>
+            <Menu.Item name='user'>
+              About
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>sally eng.ineer</Header>
+              <InputForm />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
