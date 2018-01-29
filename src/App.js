cf2c9cd78357258ec65a8d5c4d98c77e330bc9cf
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-01-28 18:31:25 -0800

    Add Semantic grids

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Grid, Image } from 'semantic-ui-react'
+
+import InputForm from './components/input_form';
+
+import './index.css';
+// import App from './App'; // following tutorial
+import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Grid>
+        <Grid.Column width={4}>
+          <div><h1>Left Column</h1></div>
+        </Grid.Column>
+
+        <Grid.Column width={9}>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={9}>
+                <div><h1>Left Column</h1></div>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={9}>
+                <div><h1>Left Column</h1></div>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Grid.Column>
+      </Grid>
+    )
   }
 }
 
-export default App;
+export default App
